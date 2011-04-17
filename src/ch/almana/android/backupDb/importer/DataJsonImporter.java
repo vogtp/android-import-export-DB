adb9b143d744e69b62418b4f7e134f941288b66d
Patrick S. Vogt <patrick.vogt@unibas.ch>
2011-04-17 20:05:23 +0200

    Import JSON files again

---
+++ src/ch/almana/android/backupDb/importer/DataJsonImporter.java
@@ -0,0 +1,48 @@
+package ch.almana.android.backupDb.importer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+import ch.almana.android.backupDb.constants.JsonConstants;
+
+public class DataJsonImporter {
+
+	public static final String LOG_TAG = "DataJsonImporter";
+
+	private JSONObject jsonDb;
+
+	public DataJsonImporter(String DbName, File directory) {
+		try {
+			jsonDb = (JSONObject) (new JSONObject(readFile(DbName, directory))).get(DbName);
+		} catch (JSONException e) {
+			Log.e(LOG_TAG, "Cannot parse as JSON", e);
+			jsonDb = new JSONObject();
+		} catch (IOException e) {
+			Log.e(LOG_TAG, "Cannot read json DB file", e);
+			jsonDb = new JSONObject();
+		}
+	}
+
+	private String readFile(String dbName, File directory) throws IOException {
+		File file = new File(directory, dbName + JsonConstants.FILE_NAME);
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			sb.append(line);
+		}
+		return sb.toString();
+	}
+
+	public JSONArray getTables(String tableName) throws JSONException {
+		return jsonDb.getJSONArray(tableName);
+	}
+
+}
---
+++ src/ch/almana/android/backupDb/importer/JSONBundle.java
@@ -0,0 +1,46 @@
+package ch.almana.android.backupDb.importer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class JSONBundle {
+
+	private static final String LOG_TAG = "JSONBundle";
+	private JSONObject jsonObject;
+
+	public JSONBundle(JSONObject jsonObject) {
+		super();
+		this.jsonObject = jsonObject;
+	}
+
+	public long getLong(String name) {
+		try {
+			return jsonObject.getLong(name);
+		} catch (JSONException e) {
+			Log.i(LOG_TAG, "Cannot read " + name + " as long", e);
+			return 0;
+		}
+	}
+
+	public String getString(String name) {
+		try {
+			return jsonObject.getString(name);
+		} catch (JSONException e) {
+			Log.i(LOG_TAG, "Cannot read " + name + " as string", e);
+			return null;
+		}
+	}
+
+	public int getInt(String name) {
+		try {
+			return jsonObject.getInt(name);
+		} catch (JSONException e) {
+			Log.i(LOG_TAG, "Cannot read " + name + " as int", e);
+			return 0;
+		}
+
+	}
+
+}
